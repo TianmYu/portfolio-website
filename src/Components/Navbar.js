abab6422bca472aa4f7b05cc731c77702861db2d
Tian Yu <tianmyu2000@gmail.com>
2023-06-09 17:09:20 -0400

    cleaned up code

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -12,13 +12,13 @@ class NavBar extends Component {
 
     componentDidMount() {
         let path = window.location.pathname;
-        let projectsActive = false
+        let projectsActive = false;
         
         if (path == "/"){
             path = "/home";
         }
         if (path == "/projects"){
-            projectsActive = true
+            projectsActive = true;
         }
         console.log(path)
         this.setState({
@@ -50,46 +50,3 @@ class NavBar extends Component {
 }
 
 export default NavBar
-    // return (
-    //     // <Navbar expand="sm">
-    //         <Nav variant="underline" defaultActiveKey="/">
-    //             <Nav.Item>
-    //                 <Nav.Link href="/">Active</Nav.Link>
-    //             </Nav.Item>
-    //             <Nav.Item>
-    //                 <Nav.Link href="/about" eventKey="about">About</Nav.Link>
-    //             </Nav.Item>
-    //             <Dropdown as={NavItem}>
-    //             <Dropdown.Toggle as={NavLink}>Click to see more…</Dropdown.Toggle>
-    //             <Dropdown.Menu>
-    //                 <Dropdown.Item>Hello there!</Dropdown.Item>
-    //             </Dropdown.Menu>
-    //             </Dropdown>
-    //         </Nav>
-    //     // </Navbar>
-    //       );
-  
-//   <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//     <a className="navbar-brand" href="/">Tian Engineers</a>
-//     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="/navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-//       <span className="navbar-toggler-icon"></span>
-//     </button>
-//     <div className="collapse navbar-collapse" id="navbarNav">
-//       <ul className="navbar-nav">
-//         <li className="nav-item dropdown">
-//           <a className="nav-link dropdown-toggle" id="navbarProjects" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> 
-//             Projects</a>
-//             <div className="dropdown-menu" aria-labelledby="navbarProjects">
-//                 <a className="dropdown-item" href="/">Project 1</a>
-//                 <a className="dropdown-item" href="/">Project 2</a>
-//                 <a className="dropdown-item" href="/">Project 3</a>
-//             </div>
-//         </li>
-
-        
-//         <li className="nav-item">
-//           <a className="nav-link" href="/about">About</a>
-//         </li>
-//       </ul>
-//     </div>
-//   </nav>
