7cbe42c28aa976466f5e7a50208877d93c05dc30
Tian Yu <tianmyu2000@gmail.com>
2023-07-04 21:42:52 -0400

    setup bottom right socials

---
+++ src/Components/Socials.js
@@ -0,0 +1,55 @@
+import {ReactComponent as FacebookLogo} from "../Assets/facebook-social-logo.svg"
+import {ReactComponent as GithubLogo} from "../Assets/GitHub-logo.svg"
+import {ReactComponent as LinkedinLogo} from "../Assets/linkedin-logo.svg"
+
+
+const Socials = () => {
+    return(
+            <div className="socials">
+                <a href="https://www.linkedin.com/in/tian-miao-yu/">
+                    <div className="socials-bubble" >
+                        <div className="text-outer">
+                            <div className="text-inner">
+                            <LinkedinLogo
+                            fill='#0E1428' // TODO: load this from CSS variable declaration
+                            style={{
+                                height:20,
+                                width:20,
+                            }}/>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+                <a  href="https://github.com/TianmYu">
+                    <div className="socials-bubble" >
+                        <div className="text-outer">
+                            <div className="text-inner">
+                            <GithubLogo
+                            fill='#0E1428'
+                            style={{
+                                height:20,
+                                width:20,
+                            }}/>
+                            </div>
+                        </div>
+                    </div>
+                </a> 
+                <a href="https://www.facebook.com/tian.yu.3975">
+                    <div className="socials-bubble" >
+                        <div className="text-outer">
+                            <div className="text-inner">
+                            <FacebookLogo
+                            fill='#0E1428'
+                            style={{
+                                height:20,
+                                width:20,
+                            }}/>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+    )
+}
+
+export default Socials
